2d78d7a238bd22b5aff65a8f642926e4b80700a6
son1318 <sonjd1318@naver.com>
2020-11-11 14:29:09 +0900

    blog객체데이터 가져오기

--- crudapp/views.py
+++ crudapp/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
+from .models import Blog
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    blogs = Blog.objects
+    return render(request, 'home.html', {'blogs':blogs})
 
 def new(request):
     full_text = request.GET['fulltext']
